9d2c666b65a0da109e301ea5d8af3b24831bd898
Igor Manakhau <mi8mto@gmail.com>
2025-12-17 16:07:37 +0300

    lesson14

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import "./App.css";
 
 export function App() {
   const [selectedTrackId, setSelectedTrackId] = useState(null);
+  const [selectedTrack, setSelectedTrack] = useState(null);
   const [tracks, setTracks] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    console.log("effect");
     fetch("https://musicfun.it-incubator.app/api/1.0/playlists/tracks", {
       headers: {
         "api-key": apiKey,
@@ -41,30 +42,77 @@ export function App() {
       <button
         onClick={() => {
           setSelectedTrackId(null);
+          setSelectedTrack(null);
         }}
       >
         reset soundtrack
       </button>
-      <ul>
-        {tracks.map((track) => (
-          <li
-            key={track.id}
-            style={{
-              border:
-                track.id === selectedTrackId ? "1px solid orange" : "none",
-            }}
-          >
-            <div
-              onClick={() => {
-                setSelectedTrackId(track.id);
-              }}
-            >
-              {track.attributes.title}
+      <div
+        style={{
+          display: "flex",
+          gap: "30px",
+        }}
+      >
+        <ul>
+          {tracks.map((track) => {
+            return (
+              <li
+                key={track.id}
+                style={{
+                  border:
+                    track.id === selectedTrackId ? "1px solid orange" : "none",
+                }}
+              >
+                <div
+                  onClick={() => {
+                    setSelectedTrackId(track.id);
+                    // setIsLoading(true);
+
+                    fetch(
+                      "https://musicfun.it-incubator.app/api/1.0/playlists/tracks/" +
+                        track.id,
+                      {
+                        headers: {
+                          "api-key": apiKey,
+                        },
+                      }
+                    )
+                      .then((res) => res.json())
+                      .then((json) => setSelectedTrack(json.data));
+                    // .finally(() => setIsLoading(false));
+                  }}
+                >
+                  {track.attributes.title}
+                </div>
+
+                <audio
+                  controls
+                  src={track.attributes.attachments[0].url}
+                ></audio>
+              </li>
+            );
+          })}
+        </ul>
+        <div>
+          <h2>Details</h2>
+          {!selectedTrack && !selectedTrackId && "Track is not selected"}
+          {!selectedTrack && selectedTrackId && "Loading..."}
+          {selectedTrack &&
+            selectedTrackId &&
+            selectedTrack.id !== selectedTrackId &&
+            "Loading..."}
+          {selectedTrack && (
+            <div>
+              <h3>{selectedTrack.attributes.title}</h3>
+              <p>
+                <h4></h4>
+                {selectedTrack.attributes.lyrics ?? "no lyrics"}
+              </p>
             </div>
-            <audio controls src={track.attributes.attachments[0].url}></audio>
-          </li>
-        ))}
-      </ul>
+          )}
+        </div>
+        
+      </div>
     </>
   );
 }
